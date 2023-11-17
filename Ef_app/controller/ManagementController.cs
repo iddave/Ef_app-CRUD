f7ba4d2052e2234afc0b69c797e687a518031a00
Давид Лалиев <laliev2001@mail.ru>
2023-11-17 17:45:05 +0300

    .

---
+++ Ef_app/controller/ManagementController.cs
@@ -0,0 +1,14 @@
+﻿using Ef_app.repository.implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ef_app.controller
+{
+    internal class ManagementController : ManagementRepositoryImpl
+    {
+
+    }
+}
